e8b18c244d1bb1ec446f2619a7de45b135d9e7b0
JhonnyNinabanda <jeninabandap@uce.edu.ec>
2025-11-26 16:24:37 -0500

    Taller 13 - Componentes

---
+++ src/components/Contador.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Universidad Central del Ecuador <br> Carrera de Computación</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
---
+++ src/views/ContadorView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Hola Mundo desde la vista Jhonny</h2>
+    <Contador />
+  </div>
+</template>
+
+<script>
+import Contador from '../components/Contador.vue'
+
+export default {
+  components: {
+    Contador,
+  }
+}
+</script>
+
+<style>
+</style>
